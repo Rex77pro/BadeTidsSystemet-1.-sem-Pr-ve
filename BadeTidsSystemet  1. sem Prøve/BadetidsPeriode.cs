3d050803ce7ae1f33e1f02e5c22640d189484f4d
Rex77pro <smaurieka@gmail.com>
2022-06-14 10:15:48 +0200

    Opg 2

---
+++ BadeTidsSystemet  1. sem Prøve/BadetidsPeriode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadeTidsSystemet__1._sem_Prøve
+{
+    class BadetidsPeriode
+    {
+        //Type string Angiver typen af badeaktivitet – f.eks. ”Morgendukkert” eller ”Aftensvaler”. Default er ”Badning”
+        //UgeDag DayOfWeek Dag i ugen hvor man må bade. Default er DayOfWeek.Sunday
+        //StartTidspunkt DateTime Default er new DateTime()
+        //SlutTidspunkt DateTime Default er new DateTime()
+
+        private string _type;
+        private DayOfWeek _ugeDag;
+        private DateTime _startTidspunkt;
+        private DateTime _slutTidsPunkt;
+
+        Dictionary<string, Kreds> kredse = new Dictionary<string, Kreds>()
+        {
+            { "HUP", new Kreds ("HUB", "HulaPigerne", "Mimersvej 4, 4600 Køge", 25)  }
+        };
+
+        public BadetidsPeriode(string Type, DayOfWeek UgeDag, DateTime Start, DateTime Slut)
+        {
+            _type = Type;
+            _ugeDag = UgeDag;
+            _startTidspunkt = Start;
+            _slutTidsPunkt = Slut;
+            
+        }
+
+        public string Type { get; set; }
+        public DayOfWeek UgeDag { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Slut { get; set; }
+
+        public override string ToString()
+        {
+            return $"Type: {_type} | Dag: {_ugeDag} | Start: {_startTidspunkt}, Slut {_slutTidsPunkt}";
+        }
+
+
+    }
+}
--- BadeTidsSystemet  1. sem Prøve/Program.cs
+++ BadeTidsSystemet  1. sem Prøve/Program.cs
@@ -9,6 +9,9 @@ namespace BadeTidsSystemet__1._sem_Prøve
             Kreds k1 = new Kreds("HUP", "HulaPigerne", "Mimersvej 4, 4600 Køge", 25);
             Console.WriteLine(k1);
 
+            BadetidsPeriode b1 = new BadetidsPeriode("Badning", DayOfWeek.Saturday, new DateTime().AddHours(6).AddMinutes(30), new DateTime().AddHours(8).AddMinutes(0));
+            Console.WriteLine(b1);
+
             Console.WriteLine("\n");
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
