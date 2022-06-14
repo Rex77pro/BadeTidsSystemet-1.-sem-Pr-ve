f4d3658febb5a63e194035d1f2a0fc57d4b45636
Rex77pro <smaurieka@gmail.com>
2022-06-14 09:39:16 +0200

    Opg 1

---
+++ BadeTidsSystemet  1. sem Prøve/Kreds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadeTidsSystemet__1._sem_Prøve
+{
+    class Kreds
+    {
+
+        //ID- string Kredsens ID
+        //Navn string Kredsens navn
+        //Adresse string Kredsens adresse
+        //AntalDeltagere int Antallet af personer kredsen deltager med
+
+        private string _id;
+        private string _navn;
+        private string _adresse;
+        private int _deltagere;
+
+        public Kreds(string Id, string Navn, string Adresse, int Deltagere)
+        {
+            _id = Id;
+            _navn = Navn;
+            _adresse = Adresse;
+            _deltagere = Deltagere;
+        }
+
+        public string Id { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public int Deltagere { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {_id} | Navn: {_navn} | Adresse: {_adresse} | Antal deltagere: {_deltagere}";
+        }
+
+    }
+}
---
+++ BadeTidsSystemet  1. sem Prøve/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BadeTidsSystemet__1._sem_Prøve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kreds k1 = new Kreds("HUP", "HulaPigerne", "Mimersvej 4, 4600 Køge", 25);
+            Console.WriteLine(k1);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+        }
+    }
+}
